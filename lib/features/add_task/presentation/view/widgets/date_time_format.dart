464297cf3aeaa175e675634667c282560bb40595
faisal <faisalabdalnasser275@gmail.com>
2024-08-20 22:41:08 +0300

    clear all tasks from hive

--- lib/features/add_task/presentation/view/widgets/date_time_format.dart
+++ lib/features/add_task/presentation/view/widgets/date_time_format.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:intl/intl.dart';
 import 'package:todoist/core/utils/constans.dart';
+import 'package:todoist/features/add_task/data/model/todoist_model.dart';
+import 'package:todoist/features/add_task/presentation/view/add_task_page.dart';
 
 class DateTimeFormat extends StatelessWidget {
   const DateTimeFormat({super.key});
@@ -40,9 +43,18 @@ class DateTimeFormat extends StatelessWidget {
                 color: bodyGray.withOpacity(1),
               ),
         
-              child: const Icon(
-                Icons.refresh,
-                color: Colors.white,
+              child: IconButton(
+                onPressed: () {
+                  var todoistBox = Hive.box<TodoistModel>(kTodoistBox);
+                  todoistBox.clear();
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context)=> const TasksPage()));
+                },
+                icon:const Icon(
+                  Icons.refresh,
+                  color: Colors.white,
+                  ),
+                
                 ),
             ),
           ],
